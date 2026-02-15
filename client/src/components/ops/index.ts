434e765e19d28ee52108b720d0626702e1d178ab
Anita Rakesh Wadhera <rakeshanita@Rakeshs-MacBook-Air.local>
2026-02-16 01:44:43 +0530

    chore: create ops components and pages directories

---
+++ client/src/components/ops/index.ts
@@ -0,0 +1,6 @@
+export { FilingStatusCard } from './FilingStatusCard';
+export { ClientInfoCard } from './ClientInfoCard';
+export { SlaCard } from './SlaCard';
+export { InternalNotesTab } from './InternalNotesTab';
+export { WorkItemsTable } from './WorkItemsTable';
+export { UnifiedTimeline } from './UnifiedTimeline';
